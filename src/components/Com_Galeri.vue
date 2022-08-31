a2ead0018a3ab8a46bc86f3f5cf202e2218801c0
Farrij-Rifai <ripaiparrij@gmail.com>
2022-08-31 08:07:53 -0700

    push Pertama

---
+++ src/components/Com_Galeri.vue
@@ -0,0 +1,78 @@
+<template>
+  <!-- Instagram Section Begin -->
+  <div class="instagram-photo">
+    <div
+      class="insta-item set-bg"
+      style="background-image: url('/img/insta-1.jpg')"
+    >
+      <div class="inside-text">
+        <i class="ti-instagram"></i>
+        <h5>
+          <a href="#">gallery 1</a>
+        </h5>
+      </div>
+    </div>
+    <div
+      class="insta-item set-bg"
+      style="background-image: url('/img/insta-2.jpg')"
+    >
+      <div class="inside-text">
+        <i class="ti-instagram"></i>
+        <h5>
+          <a href="#">gallery 2</a>
+        </h5>
+      </div>
+    </div>
+    <div
+      class="insta-item set-bg"
+      style="background-image: url('/img/insta-3.jpg')"
+    >
+      <div class="inside-text">
+        <i class="ti-instagram"></i>
+        <h5>
+          <a href="#">gallery 3</a>
+        </h5>
+      </div>
+    </div>
+    <div
+      class="insta-item set-bg"
+      style="background-image: url('/img/insta-4.jpg')"
+    >
+      <div class="inside-text">
+        <i class="ti-instagram"></i>
+        <h5>
+          <a href="#">gallery 4</a>
+        </h5>
+      </div>
+    </div>
+    <div
+      class="insta-item set-bg"
+      style="background-image: url('/img/insta-5.jpg')"
+    >
+      <div class="inside-text">
+        <i class="ti-instagram"></i>
+        <h5>
+          <a href="#">gallery 5</a>
+        </h5>
+      </div>
+    </div>
+    <div
+      class="insta-item set-bg"
+      style="background-image: url('/img/insta-6.jpg')"
+    >
+      <div class="inside-text">
+        <i class="ti-instagram"></i>
+        <h5>
+          <a href="#">gallery 6</a>
+        </h5>
+      </div>
+    </div>
+  </div>
+  <!-- Instagram Section End -->
+</template>
+
+<script>
+export default {
+  name: "Com_Galeri",
+};
+</script>
